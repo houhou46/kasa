4b31e5848c782d392b579121284bd5913609548e
houhou46 <122450870+houhou46@users.noreply.github.com>
2024-01-25 09:52:58 +0100

    ajout du header dans la page d'accueil

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home/Home";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+   
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+    
+  
